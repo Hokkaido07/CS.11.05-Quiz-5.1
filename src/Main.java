643ae068356658fcf38f7fdfa1b7cde171b18c6c
Hokkaido <Hokkaido.12307@gmail.com>
2023-11-16 12:08:22 +0800

    Update 1.0 with solutions to all prompts.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+
+/*
+This is Quiz Lab 5.1 Arrays
+Author: Alexis C.
+Date: 11-16-2023
+ */
+
+
 public class Main {
 
     /**
@@ -8,9 +16,18 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
+        int[] combinedArray = new int[array1.length + array2.length];
 
-        return null;
+        int index = 0;
+        for (int num : array1) {
+            combinedArray[index++] = num;
+        }
 
+        for (int num : array2) {
+            combinedArray[index++] = num;
+        }
+
+        return combinedArray;
     }
 
     /**
@@ -21,10 +38,18 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
+        int length = array1.length;
+        int[] zippedArray = new int[length * 2];
 
-        return null;
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            zippedArray[index++] = array1[i];
+            zippedArray[index++] = array2[i];
+        }
 
+        return zippedArray;
     }
+
 
     /**
      * Given two arrays of integers of equal length, write a method called product that multiplies each element in the first array by the element at the corresponding index in the second array.
@@ -34,8 +59,12 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
+        int[] productArray = new int[array1.length];
 
-        return null;
+        for(int i = 0;i<array1.length;i++){
+            productArray[i] = array1[i] * array2[i];
+        }
+        return productArray;
 
     }
 
@@ -56,14 +85,26 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
+        int[] capitalCounts = new int[words.length];
 
-        return null;
+        for (int i = 0; i < words.length; i++) {
+            capitalCounts[i] = countCapitalLetters(words[i]);
+        }
 
+        return capitalCounts;
     }
 
     public static int countCapitalLetters(String word) {
+        int count = 0;
 
-        return 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isAlphabetic(c) && Character.isUpperCase(c)) {
+                count++;
+            }
+        }
+
+        return count;
 
     }
 
